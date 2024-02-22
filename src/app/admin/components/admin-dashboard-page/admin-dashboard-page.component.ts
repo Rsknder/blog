ebde14e54ca3936fd91b42cf7593bf0f9faee806
Рахимов Искандер Зуфарович <iskander.rakhimov@domrf.ru>
2024-02-22 22:04:29 +0300

    updated getposts

--- src/app/admin/components/admin-dashboard-page/admin-dashboard-page.component.ts
+++ src/app/admin/components/admin-dashboard-page/admin-dashboard-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Post } from 'src/app/interfaces/interfaces';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-admin-dashboard-page',
   templateUrl: './admin-dashboard-page.component.html',
   styleUrls: ['./admin-dashboard-page.component.scss']
 })
-export class AdminDashboardPageComponent {
+export class AdminDashboardPageComponent implements OnInit, OnDestroy {
+
+
+  onRemovePost(id: string | undefined) {
+
+  }
+
+
+  posts: Post[] = [];
+  postsSub!: Subscription;
+
+  constructor(private postService: PostService) {
+
+  }
+
+
+  ngOnInit(): void {
+    this.postsSub = this.postService.getAllPostsForDS().subscribe((posts) => {
+
+      this.posts = posts
+      console.log("response from OnInint ", posts)
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub ? this.postsSub.unsubscribe() : ""
+  }
+
 
 }
